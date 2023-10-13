dc8f69becc8afd901cb042dfc27288a9208932d5
Natalia Domanska <nataliaidomanska@gmail.com>
2023-10-13 20:53:12 +0200

    config.js and helpers.js creation

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,7 @@
+// all the variables that should be constants and reused across the project
+
+// it will allow us to easily configure our project by changing some of the data in this configuration file
+
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+
+export const TIMEOUT_SEC = 10;
