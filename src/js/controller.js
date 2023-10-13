a8a50f5714ca8c697ecac85f13c889d83f919f69
Natalia Domanska <nataliaidomanska@gmail.com>
2023-10-13 19:26:23 +0200

    initial commit

---
+++ src/js/controller.js
@@ -0,0 +1,47 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading recipe
+
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+
+window.addEventListener('hashchange', controlRecipes);
+
+window.addEventListener('load', controlRecipes);
