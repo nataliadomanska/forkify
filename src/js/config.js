b327693407363e8d3b1a2e57114414b3a72e90ba
Natalia Domanska <nataliaidomanska@gmail.com>
2023-11-05 15:23:58 +0100

    implementing search results

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,6 @@
 
 // it will allow us to easily configure our project by changing some of the data in this configuration file
 
-export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 
 export const TIMEOUT_SEC = 10;
