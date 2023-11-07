1ac251507166a5cbf6ec0c6885c4671e0a52f103
Natalia Domanska <nataliaidomanska@gmail.com>
2023-11-07 19:30:07 +0100

    dom updating algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,6 +16,8 @@ const controlRecipes = async function () {
 
     if (!id) return;
     recipeView.renderSpinner();
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
 
     // 1) Loading recipe
 
@@ -65,7 +67,8 @@ const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 // Publisher-Subscriber Pattern: fx from controller (handler) is passed into the function of view (listener)
