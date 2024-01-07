243c15002a3a51a4848b8edac00107bf8eb72cc9
Natalia Domanska <nataliaidomanska@gmail.com>
2024-01-07 17:11:22 +0100

    API post, developer key, rendering

--- src/js/config.js
+++ src/js/config.js
@@ -7,3 +7,5 @@ export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 
 export const RES_PER_PAGE = 10;
+export const KEY = '1c326262-79a0-4b0e-84b9-0f3dbcc4a050';
+export const MODAL_CLOSE_SEC = 2.5;
